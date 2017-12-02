18c70270893bc732e0f4672546ee6310358c85ff
Joona Viertola <contact@joonaviertola.com>
2017-12-02 22:52:47 +0200

    App init

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          // todo: Message area
+          // todo: HP
+          // todo: DP
+          // todo: XP
+        </header>
+        <main>
+          // todo: Cards
+        </main>
+        <footer>
+          // todo: Information
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default App;
